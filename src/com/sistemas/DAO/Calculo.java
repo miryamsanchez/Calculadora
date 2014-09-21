f3a5bca0073e8016d544b6745b8632dc798a19b6
alum.fial7 <alum.fial7@FIA-LAB7D3.lim.upeu.edu.pe>
2014-09-21 12:49:31 -0500

    3ee

---
+++ src/com/sistemas/DAO/Calculo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sistemas.DAO;
+
+/**
+ *
+ * @author alum.fial7
+ */
+public class Calculo {
+    private double n1;
+    private double n2;
+
+    public Calculo(double n1, double n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+    public double suma(){return n1+n2;}
+    public double resta(){return n1-n2;}
+    public double multiplicacion(){return n1*n2;}
+    public double division(){return n1/n2;}
+        
+    }
+    
+
